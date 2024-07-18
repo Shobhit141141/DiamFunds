d51b9e9a8d6806877ec0bad97565d70826fdc79d
anshd15 <anshdoshi.d16@gmail.com>
2024-07-18 18:03:08 +0530

    Initial Setup and Tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+       <h1 className='bg-red-400'>qwerty</h1> 
+    </div>
+  )
+}
+
+export default App
